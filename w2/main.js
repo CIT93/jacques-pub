09da0d9d0ea4091fb9c076d6a653ec7135679734
ayojackay <ayojackay@gmail.com>
2024-01-17 20:11:07 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,50 @@
+const householdPoints = 8;
+const sizeOfHomePoints = 10;
+const foodChoicesPoints = 6;
+const waterConsumptionPoints = 1;
+const householdPurchasesPoints = 6;
+const wasteProducePoints = 20;
+const wasteAmountPoints = 12;
+const annualTransportationPoints = 12;
+
+const total =
+  householdPoints +
+  sizeOfHomePoints +
+  foodChoicesPoints +
+  waterConsumptionPoints +
+  householdPurchasesPoints +
+  wasteProducePoints +
+  wasteAmountPoints +
+  annualTransportationPoints;
+console.log("Total: ", total);
+
+// Alternate solution
+/*let totalAlt = householdPoints;
+totalAlt += sizeOfHomePoints;
+totalAlt += foodChoicesPoints;
+totalAlt += waterConsumptionPoints;
+totalAlt += householdPurchasesPoints;
+totalAlt += wasteProducePoints;
+totalAlt += wasteAmountPoints;
+totalAlt += annualTransportationPoints;
+console.log("Alt Total: ", totalAlt);
+*/
+
+// Intermediate solution
+/*
+const points = [
+  householdPoints,
+  sizeOfHomePoints,
+  foodChoicesPoints,
+  waterConsumptionPoints,
+  householdPurchasesPoints,
+  wasteProducePoints,
+  wasteAmountPoints,
+  annualTransportationPoints,
+];
+const totalPoints = points.reduce(
+  (previousPoint, currentPoint) => previousPoint + currentPoint,
+  0
+);
+console.log("(Intermediate) Total Points: ", totalPoints);
+*/
