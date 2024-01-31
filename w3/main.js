056f90d1f5be2a663216056bd1130a45e82663ee
ayojackay <ayojackay@gmail.com>
2024-01-30 19:06:05 -0800

    setup done

---
+++ w3/main.js
@@ -0,0 +1,59 @@
+let carbonFootprintPoints = 0;
+const numberInHousehold = 9;
+
+// if (numberInHousehold < 1) {
+//   carbonFootprintPoints = carbonFootprintPoints + 14;
+// } else if (numberInHousehold === 1) {
+//   carbonFootprintPoints = carbonFootprintPoints + 12;
+// } else if (numberInHousehold === 2) {
+//   carbonFootprintPoints = carbonFootprintPoints + 10;
+// } else if (numberInHousehold === 3) {
+//   carbonFootprintPoints = carbonFootprintPoints + 8;
+// } else if (numberInHousehold === 4) {
+//   carbonFootprintPoints = carbonFootprintPoints + 6;
+// } else if (numberInHousehold === 5) {
+//   carbonFootprintPoints = carbonFootprintPoints + 4;
+// } else if (numberInHousehold > 5) {
+//   carbonFootprintPoints = carbonFootprintPoints + 2;
+// } else {
+//   console.log("no update to points");
+// }
+
+// console.log(`Based on the number of household members (${numberInHousehold}), the points would be ${carbonFootprintPoints}`);
+
+const check =
+  numberInHousehold < 1
+    ? "less"
+    : numberInHousehold > 5
+    ? "more"
+    : numberInHousehold;
+
+switch (check) {
+  case "less":
+    carbonFootprintPoints = carbonFootprintPoints + 14;
+    break;
+  case 1:
+    carbonFootprintPoints = carbonFootprintPoints + 12;
+    break;
+  case 2:
+    carbonFootprintPoints = carbonFootprintPoints + 10;
+    break;
+  case 3:
+    carbonFootprintPoints = carbonFootprintPoints + 8;
+    break;
+  case 4:
+    carbonFootprintPoints = carbonFootprintPoints + 6;
+    break;
+  case 5:
+    carbonFootprintPoints = carbonFootprintPoints + 4;
+    break;
+  case "more":
+    carbonFootprintPoints = carbonFootprintPoints + 2;
+    break;
+  default:
+    console.log("No points provided.");
+}
+
+console.log(
+  `Based on the number of household members (${numberInHousehold}), the points would be ${carbonFootprintPoints}`
+);
