0c728f7268c2cd671348697c3cf185a23724a548
ayojackay <ayojackay@gmail.com>
2024-03-21 20:14:15 -0700

    add selector and styles for food choices

--- w11/main.js
+++ w11/main.js
@@ -6,6 +6,20 @@ import { FP } from "./fp.js";
 window.onload = renderTbl(getSS());
 
 inputs.forEach((input) => input.addEventListener("blur", validation));
+
+FORM.querySelector("#foodInput").addEventListener("blur", (e) => {
+  const parent = e.target.closest("div");
+  const sibling = parent.nextElementSibling;
+  const input = sibling.querySelector("select");
+  const label = sibling.querySelector("label");
+  if (e.target.value === "") {
+    input.disabled = true;
+    label.classList.add("disabled");
+  } else {
+    input.disabled = false;
+    label.classList.remove("disabled");
+  }
+});
 FORM.addEventListener("submit", async (e) => {
   e.preventDefault();
   const firstName = FORM.firstname.value;
