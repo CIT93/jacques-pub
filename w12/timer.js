eecb4dd2647f234c83123f0ca56d2af770941162
ayojackay <ayojackay@gmail.com>
2024-04-04 19:07:53 -0700

    base files, added logic for countdown and time

---
+++ w12/timer.js
@@ -0,0 +1,35 @@
+// 1 millisecond = 1000
+// 1 Second = 10000
+// 1 Minutes = 60000
+export const TIMERS = [
+  {
+    value: "30",
+    text: "30 seconds",
+    time: 30000,
+  },
+  {
+    value: "60",
+    text: "1 minute",
+    time: 60000,
+  },
+  {
+    value: "90",
+    text: "1 minute 30 seconds",
+    time: 90000,
+  },
+  {
+    value: "120",
+    text: "2 minutes",
+    time: 120000,
+  },
+  {
+    value: "150",
+    text: "2 minutes 30 seconds",
+    time: 150000,
+  },
+  {
+    value: "180",
+    text: "3 minutes",
+    time: 180000,
+  },
+];
